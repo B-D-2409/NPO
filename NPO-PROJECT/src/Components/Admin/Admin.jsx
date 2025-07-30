d512536fdcf19c33276d5b9c24cd0e2ca2327a40
Masterclass777 <borislav.2404g@gmail.com>
2025-07-30 23:19:28 +0300

    admin component was added

---
+++ NPO-PROJECT/src/Components/Admin/Admin.jsx
@@ -0,0 +1,11 @@
+function Admin() {
+    return (
+        <div className="admin">
+        <h1>Admin Dashboard</h1>
+        <p>Welcome to the admin panel. Here you can manage users, content, and settings.</p>
+        {/* Additional admin functionalities can be added here */}
+        </div>
+    );
+}
+
+export default Admin;
